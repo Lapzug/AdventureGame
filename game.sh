7b12f576673d1d280136bee70fcad7f277f91b82
ahmed <ahelmoudi@smail.pembinatrails.ca>
2024-10-03 13:42:17 -0500

    prompts

---
+++ game.sh
@@ -0,0 +1,14 @@
+echo "welcome to the adventure game"
+echo "choose a location to explore: forest, castle, or cave."
+read location 
+if [ "$location" == "forest" ]; then
+cat forest.txt
+elif [ "$location " == "castle" ]; then
+cat castle.txt
+elif "$location" == "castle" ]; then
+cat castle.txt
+elif [ "$location" == "cave" ]; then
+cat cave.txt
+else
+echo "invalid location."
+fi
